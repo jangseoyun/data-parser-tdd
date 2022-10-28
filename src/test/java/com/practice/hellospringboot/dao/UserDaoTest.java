9713eebe6a324e266f486514be245dbc6827b423
seoyun <may-primavera@naver.com>
2022-10-28 13:45:54 +0900

    test: userDao Test 생성

---
+++ src/test/java/com/practice/hellospringboot/dao/UserDaoTest.java
@@ -0,0 +1,11 @@
+package com.practice.hellospringboot.dao;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("TODO")
+class UserDaoTest {
+
+
+}
