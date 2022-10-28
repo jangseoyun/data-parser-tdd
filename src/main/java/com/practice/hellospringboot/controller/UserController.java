be21f82e3b6c37602b6b29985ec7e97bf5ea9760
seoyun <may-primavera@naver.com>
2022-10-28 13:37:37 +0900

    feat: userController add method 생성

---
+++ src/main/java/com/practice/hellospringboot/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.practice.hellospringboot.controller;
+
+import com.practice.hellospringboot.dao.UserDao;
+import dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("/user")
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+
+    private final UserDao userDao;
+
+    @PostMapping("/add")
+    public String saveUser(UserDto userDto) {
+        log.info("user.controller");
+    }
+}
