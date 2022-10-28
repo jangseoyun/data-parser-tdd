4668ec43e2ca7cc8c6fd23f7ac33ef57f894b424
seoyun <may-primavera@naver.com>
2022-10-28 14:11:56 +0900

    test: //TODO: userDao test code 작성중

--- src/test/java/com/practice/hellospringboot/dao/UserDaoTest.java
+++ src/test/java/com/practice/hellospringboot/dao/UserDaoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
@@ -20,26 +21,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @DisplayName("userDao test")
 class UserDaoTest {
 
+    @Autowired
     UserDao userDao;
+
     UserDto user1;
     UserDto user2;
     UserDto user3;
 
-    /*@BeforeEach
+    @BeforeEach
     void setup() throws SQLException {
-        user1 = new User(1, "seoyun", "1234");
-        user2 = new User(2, "seoseo", "1234");
-        user3 = new User(3, "yunyun", "1234");
+        user1 = new UserDto(1, "seoyun", "1234");
+        user2 = new UserDto(2, "seoseo", "1234");
+        user3 = new UserDto(3, "yunyun", "1234");
         userDao.deleteAll();
     }
-
-    @Test
-    void addAndGet() throws SQLException {
-        int id = 1;
-        userDao.add(user1);
-        User user = userDao.findById(id);
-        assertEquals("seoyun", user.getName());
-        assertEquals("1234", user.getPassword());
-    }*/
 
 }
