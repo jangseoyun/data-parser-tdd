f957fab4035da6b0ec40968d530a6e8ce6789ed7
seoyun <may-primavera@naver.com>
2022-10-28 15:12:46 +0900

    refact: userDto lombok 사용

--- src/main/java/dto/UserDto.java
+++ src/main/java/dto/UserDto.java
@@ -1,26 +1,14 @@
 package dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class UserDto {
 
     private int id;
     private String name;
     private String password;
 
-    public UserDto(int id, String name, String password) {
-        this.id = id;
-        this.name = name;
-        this.password = password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 }
