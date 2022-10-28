1e07092743b6951fd1eab91f11de97caf86fd4e7
seoyun <may-primavera@naver.com>
2022-10-28 13:36:57 +0900

    feat: userDto 생성

---
+++ src/main/java/dto/UserDto.java
@@ -0,0 +1,26 @@
+package dto;
+
+public class UserDto {
+
+    private int id;
+    private String name;
+    private String password;
+
+    public UserDto(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
