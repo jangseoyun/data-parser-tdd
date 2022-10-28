d712e44a1cf0d43c1ded1b6c8b8730c88981a650
seoyun <may-primavera@naver.com>
2022-10-28 14:29:08 +0900

    test: user save responseEntity 반환

--- src/main/java/com/practice/hellospringboot/controller/UserController.java
+++ src/main/java/com/practice/hellospringboot/controller/UserController.java
@@ -4,6 +4,8 @@ import com.practice.hellospringboot.dao.UserDao;
 import dto.UserDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +23,14 @@ public class UserController {
     public int saveUser(UserDto userDto) {
         log.info("user.controller.save");
         return userDao.save(userDto);
+    }
+
+    @PostMapping("/add2")
+    public ResponseEntity<Integer> saveUser2(UserDto userDto) {
+        log.info("user.controller.save");
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(userDao.save(userDto));
     }
 
     @DeleteMapping("/delete-all")
