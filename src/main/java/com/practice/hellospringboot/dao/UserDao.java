dd74a33667b8701e728e5d775f62f5d98b40fcec
seoyun <may-primavera@naver.com>
2022-10-28 14:02:22 +0900

    feat: userDao 초기화 변경

--- src/main/java/com/practice/hellospringboot/dao/UserDao.java
+++ src/main/java/com/practice/hellospringboot/dao/UserDao.java
@@ -1,16 +1,18 @@
 package com.practice.hellospringboot.dao;
 
 import dto.UserDto;
-import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
-
-@RequiredArgsConstructor
 public class UserDao {
 
     private final DataSource dataSource;
     private final JdbcTemplate jdbcTemplate;
+
+    public UserDao(DataSource dataSource, JdbcTemplate jdbcTemplate) {
+        this.dataSource = dataSource;
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     public int save(UserDto userDto) {
         return jdbcTemplate.update("insert into users(id, name, password) values (?, ?, ?)"
