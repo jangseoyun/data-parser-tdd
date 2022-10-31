e74f5ff289d06a34beb24e74bb678132b7b5fb10
seoyun <may-primavera@naver.com>
2022-10-31 14:14:26 +0900

    feat: hospital dto 생성

---
+++ src/main/java/com/practice/hellospringboot/domain/Hospital.java
@@ -0,0 +1,31 @@
+package com.practice.hellospringboot.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Hospital {
+    private int id;
+    private String openServiceName;
+    private int openLocalGovernmentCode;
+    private String managementNumber;
+    private LocalDateTime LicenseDate;
+    private int businessStatus;
+    private int businessStatusCode;
+    private String phone;
+    private String fullAddress;
+    private String roadNameAddress;
+    private String hospitalName;
+    private String businessTypeName;
+    private int healthcareProviderCount;
+    private int patientRoomCount;
+    private int totalNumberOfBeds;
+    private float totalAreaSize;
+}
