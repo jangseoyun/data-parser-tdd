fde95ce3f0acbe292eedbba047af784907b0c6d7
seoyun <may-primavera@naver.com>
2022-10-31 15:45:03 +0900

    refact: ReadLineContext parse try-catch

--- src/main/java/com/practice/hellospringboot/domain/parser/ReadLineContext.java
+++ src/main/java/com/practice/hellospringboot/domain/parser/ReadLineContext.java
@@ -22,7 +22,11 @@ public class ReadLineContext<T>{
         );
         String str;
         while ((str = reader.readLine()) != null) {
-            result.add(parser.parse(str));
+            try {
+                result.add(parser.parse(str));
+            } catch (Exception e) {
+                System.out.printf("에러 파일내용:%s\n", str.substring(0, 20));
+            }
         }
         reader.close();
         return result;
