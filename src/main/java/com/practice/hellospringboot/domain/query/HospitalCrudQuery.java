198a62d3e2adf203f1bac2fe48131e331e7d1b45
seoyun <may-primavera@naver.com>
2022-11-01 13:26:01 +0900

    refact: hospital insert 쿼리 수정

--- src/main/java/com/practice/hellospringboot/domain/query/HospitalCrudQuery.java
+++ src/main/java/com/practice/hellospringboot/domain/query/HospitalCrudQuery.java
@@ -8,12 +8,11 @@ public class HospitalCrudQuery implements CrudQuery {
     @Override
     public String save() {
         String query = "";
-        query = "insert into nation_wide_hospitals (id, open_service_name, open_local_government_code, management_number, license_date,\\n\" +\n" +
-                "                \"                                   business_status, business_status_code, phone, full_address, road_name_address,\\n\" +\n" +
-                "                \"                                   hospital_name, business_type_name, healthcare_provider_cnt, patient_room_cnt,\\n\" +\n" +
-                "                \"                                   total_number_of_beds, total_area_size)\\n\" +\n" +
-                "                \"values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
-
+        query = "insert into nation_wide_hospitals (id, open_service_name, open_local_government_code, management_number, license_date,"
+                + " business_status, business_status_code, phone, full_address, road_name_address,"
+                + " hospital_name, business_type_name, healthcare_provider_cnt, patient_room_cnt,"
+                + " total_number_of_beds, total_area_size)"
+                + " values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
         return query;
     }
 }
