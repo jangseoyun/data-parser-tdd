7bc1cd93b7e6256e47f6980fc0fdffada12e5bff
seoyun <may-primavera@naver.com>
2022-11-01 15:05:32 +0900

    test: hospitalDao findById test code 완료

--- src/test/java/com/practice/hellospringboot/domain/parser/HospitalParserTest.java
+++ src/test/java/com/practice/hellospringboot/domain/parser/HospitalParserTest.java
@@ -43,8 +43,8 @@ class HospitalParserTest {
         assertEquals("광주광역시 북구 동문대로 24, 3층 (풍향동)", hospital.getRoadNameAddress());//col:19
         assertEquals("효치과의원", hospital.getHospitalName());//col:21
         assertEquals("치과의원", hospital.getBusinessTypeName());//col:25
-        assertEquals(1, hospital.getHealthcareProviderCount()); //col:30
-        assertEquals(0, hospital.getPatientRoomCount()); //col:31
+        assertEquals(1, hospital.getHealthcareProviderCnt()); //col:30
+        assertEquals(0, hospital.getPatientRoomCnt()); //col:31
         assertEquals(0, hospital.getTotalNumberOfBeds()); //col:32
         assertEquals(52.29f, hospital.getTotalAreaSize()); //col:33
 
@@ -64,12 +64,14 @@ class HospitalParserTest {
         }
     }
 
-    @DisplayName("save")
+    @DisplayName("save and getHospitalOne")
     @Test
-    void save() {
+    void saveAndGetOne() {
         HospitalParser hp = new HospitalParser();
         Hospital hospital = hp.parse(line1);
 
-        hospitalDao.save(hospital);
+        assertEquals(1, hospitalDao.save(hospital));
+        Hospital byId = hospitalDao.findById(1);
+        assertEquals("PHMA119993620020041100004", byId.getManagementNumber());
     }
 }
