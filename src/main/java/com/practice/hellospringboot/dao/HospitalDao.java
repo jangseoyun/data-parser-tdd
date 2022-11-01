4d502b30337c21900ce5b4a0f8c02ae93174c219
seoyun <may-primavera@naver.com>
2022-11-01 11:39:27 +0900

    feat: hospitalDao save

--- src/main/java/com/practice/hellospringboot/dao/HospitalDao.java
+++ src/main/java/com/practice/hellospringboot/dao/HospitalDao.java
@@ -1,4 +1,24 @@
 package com.practice.hellospringboot.dao;
 
+import com.practice.hellospringboot.domain.Hospital;
+import com.practice.hellospringboot.domain.query.CrudQuery;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
 public class HospitalDao {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final CrudQuery crudQuery;
+
+    public int save(Hospital hospital) {
+        int saveResult = jdbcTemplate.update(crudQuery.save()
+                , hospital.getId(), hospital.getOpenServiceName(), hospital.getOpenLocalGovernmentCode(), hospital.getManagementNumber(), hospital.getLicenseDate()
+                , hospital.getBusinessStatus(), hospital.getBusinessStatusCode(), hospital.getPhone(), hospital.getFullAddress(), hospital.getRoadNameAddress()
+                , hospital.getHospitalName(), hospital.getBusinessTypeName(), hospital.getHealthcareProviderCount(), hospital.getPatientRoomCount(), hospital.getTotalNumberOfBeds()
+                , hospital.getTotalAreaSize());
+        return saveResult;
+    }
 }
