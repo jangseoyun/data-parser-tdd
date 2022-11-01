d4ccc95c697f6561e542e3bb795ab9c784d594da
seoyun <may-primavera@naver.com>
2022-11-01 15:38:02 +0900

    commit: hospitalDao 주석 제거

--- src/main/java/com/practice/hellospringboot/dao/HospitalDao.java
+++ src/main/java/com/practice/hellospringboot/dao/HospitalDao.java
@@ -26,23 +26,7 @@ public class HospitalDao {
                 , hospital.getTotalAreaSize());
         return saveResult;
     }
-
-    /*private int id;/
-    private String openServiceName;/
-    private int openLocalGovernmentCode;/
-    private String managementNumber;/
-    private LocalDateTime LicenseDate;/
-    private int businessStatus;/
-    private int businessStatusCode;/
-    private String phone;/
-    private String fullAddress;/
-    private String roadNameAddress;/
-    private String hospitalName;/
-    private String businessTypeName;/
-    private int healthcareProviderCount;/
-    private int patientRoomCount;/
-    private int totalNumberOfBeds;/
-    private float totalAreaSize;*/
+    
     public Hospital findById(int id) {
         RowMapper<Hospital> rowMapper = new RowMapper<>() {
             @Override
